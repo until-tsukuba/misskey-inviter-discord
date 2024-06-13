9559702b9837a9a7b8196dc23d8fd7242a7e470b
Nakaya <eniehack@outlook.jp>
2024-06-13 21:02:34 +0900

    fix: バグ消えて動いた

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from discord import app_commands
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 MISSKEY_TOKEN = os.environ.get("MISSKEY_TOKEN")
 MISSKEY_HOST = os.environ.get("MISSKEY_HOST")
-MISSKEY_HOST_URL = os.environ.get("MISSKEY_HOST_URL")
 
 intents = discord.Intents.default()
-client = discord.Client(intents)
+client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 @client.event
@@ -20,15 +19,20 @@ async def on_ready():
 
 @tree.command(name="invite", description=f"{MISSKEY_HOST}への招待トークンを作成します")
 async def invite(interaction: discord.Interaction):
-    resp = requests.post(f"{MISSKEY_HOST_URL}/api/invite/create", headers={
-        "Authorization": f"Bearer {MISSKEY_TOKEN}"
-        })
+    await interaction.response.defer()
+    resp = requests.post(f"https://{MISSKEY_HOST}/api/admin/invite/create", headers={
+        "Authorization": f"Bearer {MISSKEY_TOKEN}",
+        }, json={})
     if resp.status_code != 200:
-        await interaction.response.send('エラーです！')
+        await interaction.followup.send('エラーです！')
         print(f"token create err. statuscode: {resp.status_code}. payload: {resp.json()}", file=sys.stderr)
         return
-    token = resp.json()
-    await interaction.response.send(f'招待トークンは「{token["token"]}」です！\n{MISSKEY_HOST_URL} にアクセスして登録してください')
+    tokens = resp.json()
+    token = tokens[0]
+    if token["expiresAt"] is None:
+        await interaction.followup.send(f'招待トークンは「{token["code"]}」です！ https://{MISSKEY_HOST} にアクセスして登録してください')
+    else:
+        await interaction.followup.send(f'招待トークンは「{token["code"]}」です！{token["expiresAt"]}まで https://{MISSKEY_HOST} にアクセスして登録してください')
 
 # Botを実行
 client.run(DISCORD_TOKEN)
